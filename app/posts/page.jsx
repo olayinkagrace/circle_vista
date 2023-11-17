bb3ed6e9cf4d7d12045f2ba31138bbb4b01e86f2
Olayinka <olayinka4grace@gmail.com>
2023-11-17 10:59:50 +0100

    users can view post added

---
+++ app/posts/page.jsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import axios from 'axios';
+import { selectUsername } from '../redux/slices/userSlice';
+import Post from '../components/Post';
+import { FaHome } from 'react-icons/fa';
+import Link from 'next/link';
+
+export default function Posts() {
+  const [userPosts, setUserPosts] = useState([]);
+  const username = useSelector(selectUsername);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchUserPosts = async () => {
+      try {
+        if (username) {
+          const response = await axios.get(`https://assignment-api-spxd.onrender.com/api/posts/${username}`);
+          setUserPosts(response.data);
+        }
+      } catch (error) {
+        console.error('Error fetching user posts', error);
+        // Handle the error, e.g., display an error message to the user
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchUserPosts();
+  }, [username]);
+
+
+
+  return (
+    <main className="bg-purple-500 min-h-screen  flex justify-center">
+      <div className="bg-white p-8 rounded-lg shadow-md w-[80%]  my-10 ">
+        <div className='flex flex-wrap justify-between'>
+        <h1 className="text-3xl font-bold mb-4 text-purple-800">Your Posts</h1>
+       <Link href="/"> <FaHome className='text-violet-400' /></Link>
+        </div>
+       
+        {loading ? (
+          <p className="text-gray-800">Loading...</p>
+        ) : Array.isArray(userPosts.data) && userPosts.data.length > 0 ? (
+          <ul className="list-disc pl-4">
+            {userPosts.data.map((post, index) => (
+              <li  key={index} className="mb-4">
+                <Post
+                  key={index}
+                  post={post.post}
+                  username={post.username}
+                  base64str={post.base64str}
+                  created_at={post.created_at}
+                />
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p className="text-gray-800">No posts found for {username ? username : 'you'}</p>
+        )}
+      </div>
+    </main>
+  );
+}
